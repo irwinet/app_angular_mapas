4803a9d7be38ca2aa955cd88b1b40dc4d683e65b
irwinet <irwinestradatorres@gmail.com>
2020-12-07 12:06:32 -0500

    Configure API KEY Google Maps

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  lat = -12.1748632;
+  lng = -76.9645651;
+
   constructor() { }
 
   ngOnInit(): void {
